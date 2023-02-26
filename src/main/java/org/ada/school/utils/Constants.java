e2edeb36c185ed52e3fbe325d162d0c75edacc62
derjasai <elholaquehace88@gmail.com>
2023-02-26 16:32:36 -0500

    Primer commit

---
+++ src/main/java/org/ada/school/utils/Constants.java
@@ -0,0 +1,11 @@
+package org.ada.school.utils;
+
+public interface Constants {
+
+    String CLAIMS_ROLES_KEY = "ada_roles";
+
+    int TOKEN_DURATION_MINUTES = 1440;
+
+    String COOKIE_NAME = "cookie_name";
+
+}
